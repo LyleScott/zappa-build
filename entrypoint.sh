0d79e2e0ce5d44433f29d97e72a0575417b88fa3
Lyle Scott, III <lyle@ls3.io>
2020-05-26 00:03:35 -0400

    better comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,21 @@ if [[ -z ${EnvName} ]]; then
     exit 1
 fi
 
-find . -name __pycache__ | xargs rm -rf
-find . -name \*.pyc | xargs rm -f
-
+# Cleanup (in the Docker image only)
+find . -name __pycache__ -o -name \*.pyc | xargs rm -rf
 python -m compileall -q .
+
+# Zappa forces you into a venv. There's an option to not
+# use it, but there are bugs.
 source /venv/bin/activate
 
+# Since we are using the slim_handler, this creates 2 files:
+# - A .zip that is the code to bootstrap lambda and
+#   downloads/extracts the .tar.gz
+# - A .tar.gz that is the full code of the fatty lambda that
+#   has been sent to S3 to side-load.
 zappa package
-mv *-zappa-${EnvName}-*.* /zbuilds
 
+# Move build artifacts to a directory that is mounted on the
+# host so that they are available to zappa commands on the host.
+mv *-zappa-${EnvName}-*.* /zbuilds
